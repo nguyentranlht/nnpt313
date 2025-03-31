3e5e3941a8a989dcc0ae41f5ae187a3128514384
Nguyen Tran <nguyentranlht@gmail.com>
2025-03-31 10:56:48 +0700

    first commit 1

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/nnpt313')
+mongoose.connect('mongodb://localhost:27017/S2')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB:', err));
 
